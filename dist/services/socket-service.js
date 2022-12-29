03a924c8fc1c3a34a8c7b81ae6c668e59d39df5d
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-12-29 14:45:00 -0800

    starting on game socket logic

--- dist/services/socket-service.js
+++ dist/services/socket-service.js
@@ -1,15 +1,30 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const SocketService = (socket) => {
-    const handleEvent = () => {
-        console.log('event recieved');
-    };
-    const initialize = (username) => {
-        console.log(username);
+    const initialize = (user_id) => {
+        console.log(user_id);
         socket.emit("initializationConfirmation", "success");
     };
-    socket.on('event', handleEvent);
+    const joinGame = ({ user_id, game_id }) => __awaiter(void 0, void 0, void 0, function* () {
+        if (game_id == "fake") {
+            const responseObj = { "successful": true, identifier: { game_id: "fake_to_be_created", currentQuestion: 0, num_questions: 5 } };
+            socket.emit('gameIdentifier', responseObj);
+        }
+        else {
+            socket.emit('gameIdentifier', { "successful": false });
+        }
+    });
     socket.on('initialize', initialize);
+    socket.on('join-game', joinGame);
     socket.on('disconnect', () => console.log('a user disconnected'));
 };
 exports.default = SocketService;
--- src/services/socket-service.ts
+++ src/services/socket-service.ts
@@ -1,17 +1,28 @@
 import { Socket } from 'socket.io';
 
-const SocketService = (socket: Socket) => {
-  const handleEvent = () => {
-    console.log('event recieved');
-  };
+interface joinIdentifier {
+  user_id : string,
+  game_id: string
+}
 
-  const initialize = (username : string) => {
-    console.log(username)
+const SocketService = (socket: Socket) => {
+
+  const initialize = (user_id : string) => {
+    console.log(user_id)
     socket.emit("initializationConfirmation", "success")
   }
 
-  socket.on('event', handleEvent);
+  const joinGame = async ({user_id, game_id} : joinIdentifier) => {
+    if(game_id == "fake") {
+      const responseObj = {"successful" : true, identifier: {game_id: "fake_to_be_created", currentQuestion: 0, num_questions: 5}};
+      socket.emit('gameIdentifier', responseObj);
+    } else {
+      socket.emit('gameIdentifier', {"successful" : false});
+    }
+  }
+
   socket.on('initialize', initialize)
+  socket.on('join-game', joinGame)
   socket.on('disconnect', () => console.log('a user disconnected'));
 };
 
